fa01aa17e351e75c9c8ac0787c5bc4985faad428
jun <sangjun.bak99@gmail.com>
2022-11-10 13:09:52 -0500

    Updated for new leetcode redesign

--- hide-script.js
+++ hide-script.js
@@ -1,25 +1,21 @@
 const hideText = "Show";
 const hideColor = "#455a64";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const difficultyTag =
-    document.querySelector('[diff="easy"]') ||
-    document.querySelector('[diff="medium"]') ||
-    document.querySelector('[diff="hard"]');
-});
-
 function handleDifficultyTag(node) {
   const initialText = node.innerText;
   const initialColor = node.style.color;
+  const initialBackgroundColor = node.style["background-color"];
 
   function revealTag() {
     node.innerText = initialText;
     node.style.color = initialColor;
+    node.style["background-color"] = initialBackgroundColor;
   }
 
   function hideTag() {
     node.innerText = hideText;
     node.style.color = hideColor;
+    node.style["background-color"] = "#ffffff";
   }
 
   hideTag();
@@ -32,19 +28,41 @@ function handleDifficultyTag(node) {
   });
 }
 
-var observer = new MutationObserver(function (mutations, me) {
-  const difficultyTag =
-    document.querySelector('[diff="easy"]') ||
-    document.querySelector('[diff="medium"]') ||
-    document.querySelector('[diff="hard"]');
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", afterDOMLoaded);
+} else {
+  afterDOMLoaded();
+}
+
+function afterDOMLoaded() {
+  //Everything that needs to happen after the DOM has initially loaded.
+  const easyTag = document.evaluate(
+    "//div[text()='Easy']",
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+
+  const mediumTag = document.evaluate(
+    "//div[text()='Medium']",
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+
+  const hardTag = document.evaluate(
+    "//div[text()='Hard']",
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+
+  const difficultyTag = easyTag || mediumTag || hardTag;
 
   if (difficultyTag) {
     handleDifficultyTag(difficultyTag);
-    me.disconnect();
   }
-});
-
-observer.observe(document, {
-  childList: true,
-  subtree: true,
-});
+}
